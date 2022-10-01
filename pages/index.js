3bc0182da55b70324bbf76150308f62aacaa09c1
adamya-laptop <kumar.adamya2018@gmail.com>
2022-10-01 16:22:34 +0530

    product PAge

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,19 @@
 import React from 'react'
+import Footer from '../components/Footer/Footer'
 import HeroBanner from '../components/HeroBanner'
+import Product from '../components/Product'
 import { client } from '../lib/client'
 
 const index = ({products, bannerData}) => {
   return (
     <>
     <HeroBanner heroBanner={bannerData.length && bannerData[0]}/>
-    
+    <div className='products-heading'>
+      <h1>Best Seller Products</h1>
+      <p> speakers There are many Variations passages</p>
+    </div>
+    <div className="product-container"> {products?.map((product)=><Product key={product._id} product={product}/>)}</div>
+ <Footer/>
     </>
   )
 }
